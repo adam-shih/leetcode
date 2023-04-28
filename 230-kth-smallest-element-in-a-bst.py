d1483572eaf934819092186e353f1f23bb01df8d
Adam <shihadam1@gmail.com>
2023-04-27 23:30:09 -0700

    230. Kth Smallest Element in a BST

---
+++ 230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,13 @@
+def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    stack = []
+    cur = root
+    while stack or cur: 
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        cur = stack.pop()
+    
+        k -= 1
+        if k == 0:
+            return cur.val
+        cur = cur.righ
